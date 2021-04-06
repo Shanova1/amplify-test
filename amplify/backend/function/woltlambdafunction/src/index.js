c2a2b00acf9f4fb241e9fdf253ece346b40254d5
Your Name <you@example.com>
2021-04-06 18:18:02 +0300

    add api request

---
+++ amplify/backend/function/woltlambdafunction/src/index.js
@@ -0,0 +1,15 @@
+
+
+exports.handler = async (event) => {
+    const body = {
+        message: "Hello from Lambda"
+    }
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(body),
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+        }
+    };
+    return response;
+  };
--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { API } from 'aws-amplify';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { listNotes } from './graphql/queries';
+import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
+import { API } from 'aws-amplify';
 
 function App() {
+
+  const response = await API.get('nodeapi', '/hello');
+  
   return (
     <div className="App">
-      <header>
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>We now have Auth!</h1>
-      </header>
-      <AmplifySignOut />
+      <h1>{response}</h1>
+      {/* <AmplifySignOut /> */}
     </div>
   );
 }
